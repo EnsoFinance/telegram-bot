09320636aadda803f00fae38b7221283444c1842
Ben Wolf <benjamineliaswolf@gmail.com>
2023-08-31 16:38:25 +0200

    Update tenderly util

--- api/tenderly.ts
+++ api/tenderly.ts
@@ -1,23 +1,32 @@
-dotenv.config();
-const { TENDERLY_USER, TENDERLY_PROJECT, TENDERLY_ACCESS_KEY } = process.env;
-import { axois } from 'axios'
+import axios from "axios";
+import { BigNumberish } from "ethers";
 
-const SIMULATE_API = `https://api.tenderly.co/api/v1/account/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/simulate`
+const SIMULATE_URL =
+  "https://api.tenderly.co/api/v1/account/enso-finance/project/enso-shortcuts/simulate";
+const BASE_SHARE_URL =
+  "https://dashboard.tenderly.co/public/enso-finance/enso-shortcuts/simulator";
+export async function simulate(
+  network_id: number,
+  from: string,
+  to: string,
+  data: string,
+  value: BigNumberish
+) {
+  const transaction = {
+    network_id,
+    from,
+    input: data,
+    to,
+    value,
+    save: true,
+  };
 
-async function execute(chainId, from, to, data, gasLimit, gasPrice, value) {
-    const transaction = {
-        network_id: '1',
-        from: "0x0000000000000000000000000000000000000000",
-        input: data,
-        to,
-        // tenderly specific
-        save: true
-    }
-
-    const opts = {
-        headers: {
-            'X-Access-Key': process.env.TENDERLY_ACCESS_KEY || "",
-        }
-    const resp = await axios.post(SIMULATE_API, transaction, opts);
+  const opts = {
+    headers: {
+      // Token will be invalid after 01.09.2023
+      "X-Access-Key": "nhpAm5C6ip56u1GHKRqetZjPnFfqWJDc",
+    },
+  };
+  const resp = await axios.post(SIMULATE_URL, transaction, opts);
+  return `${BASE_SHARE_URL}/${resp.data.simulation.id}`;
 }
-console.log(resp.data);
