50bb1b5f4c38077f729c1cd3f59a24a609daa8ff
milonite <milonite@hotmail.it>
2023-09-10 15:22:10 +0200

    update fork id

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const TENDERLY_FORK_ID = "1550a58e-3004-49dd-8170-4481f953c9b9";
+export const TENDERLY_FORK_ID = '6e1abade-5161-4a27-aac2-77dfd716fb04'
 
-export const SAFE_WALLET = "0x2EaC920c002c9f83f278a75b56137CFc091a1F08";
-export const SAFE_OWNER = "0x93621DCA56fE26Cdee86e4F6B18E116e9758Ff11";
+export const SAFE_WALLET = '0x2EaC920c002c9f83f278a75b56137CFc091a1F08'
+export const SAFE_OWNER = '0x93621DCA56fE26Cdee86e4F6B18E116e9758Ff11'
 
-export const ETH = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+export const ETH = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
